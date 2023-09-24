e0772afc35ffbd93022b22a5a333e996c59c6c05
XGM <109265751+xgmxgm@users.noreply.github.com>
2023-09-24 12:41:40 +0600

    Advice Generator App

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <h1>Hello Next JS</h1>
+  )
+}
---
+++ src/widgets/Header/ui/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './Header.module.scss'
+
+export const Header = () => {
+    return (
+        <div className={styles.Header}>
+            Header
+        </div>
+    )
+}
